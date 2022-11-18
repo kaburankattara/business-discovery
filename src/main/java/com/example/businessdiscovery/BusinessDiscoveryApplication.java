91d34353394fbf569a8fd3aa6b35a2a690e190fd
tmpuser-at-498588106976 <>
2022-11-18 12:58:11 +0900

    プロジェクトをspring boot化

---
+++ src/main/java/com/example/businessdiscovery/BusinessDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.businessdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/businessdiscovery/BusinessDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
