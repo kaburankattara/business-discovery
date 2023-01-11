c5e8c43153d283ef8d7cf9fc72e20540fee3b336
kaburankattara <kaburankattara@gmail.com>
2023-01-11 09:09:28 +0900

    docker起動時、cassandraセットアップでcassandraの起動待ちをsleepから接続確認のループに変更

--- docker/volumes/cassandra-for-setup/tmp/container-init.sh
+++ docker/volumes/cassandra-for-setup/tmp/container-init.sh
@@ -3,10 +3,21 @@ if [ ! -e '/check' ]; then
     # 初回起動時に実行させたいコマンドをここに書く
     echo "セットアップ"
 
-    sleep 100
+    isLoop="true"
+    while [ $isLoop = "true" ];
+    do
+      sleep 10
+
+      cqlsh cassandra1 -e "describe keyspaces;"
+      if [ $? -eq 0 ]; then
+        echo "cassandraに接続できるようになりました。"
+        isLoop=false
+      else
+        echo "まだcassandraに接続出来ません。"
+      fi
+    done
 
     echo setup cassandra
-
     cqlsh cassandra1 -f /tmp/setup.cql
 
     echo "セットアップ完了"
